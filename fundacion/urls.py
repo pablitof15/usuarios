fac3dd32f6358cea5eede46daa7decda6e4cdc29
Pablo Flea <pablitof13@gmail.com>
2022-08-19 19:54:00 -0300

    proyecto con carpeta usuario

---
+++ fundacion/urls.py
@@ -0,0 +1,21 @@
+
+from re import template
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+from django.views.generic.base import TemplateView
+#from django.urls import path, reverse_lazy
+from django.urls import include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.inicio, name= "home"),
+    path('iniciar-sesion/',auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    path('cerrar-sesion/',auth_views.logout_then_login, name="logout"),
+
+
+
+        #includes
+    path("usuarios/",include("usuarios.urls"))
+]
---
+++ usuarios/forms.py
@@ -0,0 +1,9 @@
+from socket import fromshare
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import Usuario
+
+class UsuarioRegistroForm(UserCreationForm):
+    class Meta:
+        model=Usuario
+        fields=["first_name","last_name","username","password1","password2"]
